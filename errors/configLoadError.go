3cdb35b1d62f40ad200cb23a328f7a68c4e2b716
Alexey Maltsev <axel.user.perm@gmail.com>
2017-08-26 20:45:32 +0500

    Added method for config file reading

--- errors/configLoadError.go
+++ errors/configLoadError.go
@@ -7,14 +7,14 @@ import (
 // ConfigLoadError is for errors during loading and reading Parser`s config
 type ConfigLoadError struct {
 	Message    string
-	InnerError *error
+	InnerError error
 }
 
-func (e *ConfigLoadError) Error() string {
+func (e ConfigLoadError) Error() string {
 	return fmt.Sprintf("Could not load parser`s config: %s", e.Message)
 }
 
 // NewConfigLoadError creates ConfigLoadError
-func NewConfigLoadError(message string, inner *error) ConfigLoadError {
+func NewConfigLoadError(message string, inner error) ConfigLoadError {
 	return ConfigLoadError{Message: message, InnerError: inner}
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,11 @@
 package parser
 
 import (
+	"encoding/json"
+	"fmt"
+	"gowork/errors"
 	"gowork/models"
+	"io/ioutil"
 )
 
 func load() {
@@ -12,11 +16,19 @@ func save() {
 
 }
 
-func getParserConfig(configPath string) *models.ParserConfig error {
-	file, err := os.Open("file.go") // For read access.
-	if err != nil {
-		
+func getParserConfig(configPath string) (*models.ParserConfig, error) {
+	raw, fsErr := ioutil.ReadFile(configPath)
+	if fsErr != nil {
+		return nil, errors.NewConfigLoadError(fmt.Sprintf("Could not read file '%s'", configPath), fsErr)
 	}
+
+	var config *models.ParserConfig
+	jsonErr := json.Unmarshal(raw, config)
+	if jsonErr != nil || config == nil {
+		return nil, errors.NewConfigLoadError(fmt.Sprintf("Could not unmarshal json '%s'", configPath), jsonErr)
+	}
+
+	return config, nil
 }
 
 //Start loading data
